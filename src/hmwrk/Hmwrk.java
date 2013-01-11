bfe5e7ed35c51ddbeb595c4247029ca3240021ef
Jordan Georgiev Georgiev <jordashiro@gmail.com>
2013-01-11 08:58:13 +0200

    Revert "Revert "init""
    This reverts commit 79334c7507b2cc69152347f91085eecac9268dc9.

---
+++ src/hmwrk/Hmwrk.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hmwrk;
+
+/**
+ *
+ * @author jordashiro
+ */
+public class Hmwrk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
